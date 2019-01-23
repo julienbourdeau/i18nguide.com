b5ebf143b93c3a4e86d854059a022f34d88524bb
Julien Bourdeau <julien@sigerr.org>
2019-01-23 08:37:58 +0100

    Tailwind + basic setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.base')
+
+@section('content')
+
+  <div class="container">
+    <h1>Welcome</h1>
+
+    <p>
+      This website aims to be an easy-to-read, quick reference for internationalization best
+      practices and authoritative content around the Web.
+    </p>
+
+    <p>
+      There is no canonical way to international software. At some point you will want your software
+      to go further and you’ll start internationalizing it. It’s hard to know which way to do it
+      because there are so many ways. This website will not tell you which tools to use, but
+      instead offer suggestions for multiple options, when possible explaining the differences in
+      approach and use-case.
+    </p>
+
+	  <p>
+      This is a living document and will continue to be updated with more helpful information and
+		  examples as they become available.
+	  </p>
+
+  </div>
+
+@endsection
