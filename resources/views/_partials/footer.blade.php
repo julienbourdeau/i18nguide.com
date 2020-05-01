2ea4ac6185203e24f2f6aec4d6f1b4fa066176f1
Julien Bourdeau <julien@sigerr.org>
2020-05-01 10:29:06 +0200

    make it a static site

--- resources/views/_partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer id="footer" class="container mt-20 mb-8 text-sm text-center">
-	<div class="text-grey">
-		<a class="text-grey" href="https://www.i18nguide.com">www.i18nguide.com</a>
-		by
-		<a class="text-grey" href="http://www.wapapum.com">Alex Even</a>
-		and
-		<a class="text-grey" href="https://www.sigerr.org">Julien Bourdeau</a>
-	</div>
-</footer>
